6607061fac886d3a572f5ea3da9fdd17fa5e8ce3
Joel Villarreal II <joel.villa2329@gmail.com>
2026-02-18 09:26:52 -0700

    vector that defines hyperplane of dimension d

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ SEED = 1
 """
 Label n random samples from d-dimensions, in the hypercube that has edge lengths 2u, 
 and is centered about the origin
-1. Randomly generate a hyperplane of dimension d-1 (passes through origin for simplicity)
+1. Randomly generate a vector which defines a d-1  dimension hyperplane (passes
+ through origin for simplicity)
 2. Randomly generate a point (uniform distribution) in the hyper cube and classify it 
 via the hyperplane
 3. Output all randomly generated samples and their classifications
 """
 def make_classification(n, d = 2, u = 1):
     # Random number generator
+    # TODO: Vector, and check if stuff is greater than or less than zero
     rng = np.random.default_rng(seed=SEED) 
 
     # The coefficients defining the hyperplane
-    a = rng.random(size=(d - 1)) 
-    a = (a * 2) - 1
+    a = rng.random(size=d) 
+
+    # Vector a can have values [-1, 1)
+    a = (a * 2) - 1 
 
     # Output variables
     x = rng.random(size=(n, d)) # n randomly generated points (uniform distribution [0, 1)) 
@@ -36,9 +40,9 @@ def make_classification(n, d = 2, u = 1):
     # Classify n samples
     for i in range(n):
         total = 0
-        for j in range(d-1):
+        for j in range(d):
             total += x[i, j] * a[j]
-        if total > x[i, d - 1]:
+        if total > 0:
             #  Above hyper plane
             y[i] = 1
         # print(f"x[i] = {x[i]}, total = {total}, y[i] = {y[i]}")
