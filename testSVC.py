b2fe190442d14ce7f5c4d6b9937e6589bb279d92
charl024 <comaoeng2003@gmail.com>
2026-02-21 15:04:26 -0700

    added a way to test for C values

--- testSVC.py
+++ testSVC.py
@@ -16,13 +16,25 @@ def test(d_max):
             # print(f"y: {y}")
             # print(f"y_hat: {y_hat}")
     
+def get_accuracy(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    return np.mean(y_true == y_pred)
+
+def test_C_values():
+    Cs = [1, 5, 10, 15, 20, 50, 100, 200, 500, 1000]
+    X, y, a = make_classification(100, rand_seed= 10)
+    for C_val in Cs:
+        lvc = LinearSVC(n_iter=1000, eta=0.01)
+        lvc.fit(X, y, C=C_val)
+        y_hat = lvc.predict(X)
+        acc = get_accuracy(y, y_hat)
+
+        print(f"C: {C_val}, Training accuracy: {acc:.4f}")
+
+def generate_random_datasets():
+    pass
 
 if __name__ == "__main__":
-    test(100)
-    # # i = 1
-    # X, y = make_classification(  10, rand_seed= 10)
-    # print(y)
-    # lvc = LinearSVC()
-    # lvc.fit(X, y)
-    # y_hat = lvc.predict(X)
-    # print(y_hat)
+    test_C_values()
