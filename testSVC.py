6fe23ad44f83f0a9bb1834509f36f2aa90f2ec59
Joel Villarreal II <joel.villa2329@gmail.com>
2026-02-21 18:00:16 -0700

    part 4, partially working, need to find out why not converging

--- testSVC.py
+++ testSVC.py
@@ -1,8 +1,15 @@
-from LinearSVC import LinearSVC
+from sklearn.svm import LinearSVC
+from LinearSVC import LinearSVC as ImplementedSVC
 from generator import make_classification
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+
+N_ITER = 10
+STEP = N_ITER // 10
+TOLERANCE = 0.0001
+C = 15
+
 
 ds = [10, 50, 100]
 ns = [50, 5000, 50000]
@@ -55,7 +62,7 @@ def generate_random_datasets(seed_input=11, u_input=1):
             d50n500, d50n5000, d50n50000, 
             d100n500, d100n5000, d100n50000]
 
-def test_svc_loss_convergence_and_time(datasets, C=15, eta=0.01, n_iter=100):
+def test_svc_loss_convergence_and_time(datasets):
     idx_count = 0
     results = {}
 
@@ -63,9 +70,9 @@ def test_svc_loss_convergence_and_time(datasets, C=15, eta=0.01, n_iter=100):
         for n in ns:
             X, y, _, _, _ = datasets[idx_count]
 
-            lvc = LinearSVC(eta=eta, n_iter=n_iter)
+            lvc = ImplementedSVC(eta=TOLERANCE, C=C, n_iter=N_ITER)
             start = time.perf_counter()
-            lvc.fit(X=X, y=y, C=C)
+            lvc.fit(X=X, y=y)
             elapsed = time.perf_counter() - start
 
             results[(d, n)] = {
@@ -77,9 +84,56 @@ def test_svc_loss_convergence_and_time(datasets, C=15, eta=0.01, n_iter=100):
 
     return results
 
-def plot_loss_grid(results):
+def hinge_loss(y, y_hat, w, C):
+    vals = 1 - y * y_hat
+
+    # If vals[i] is less than zero, put zero, otherwise keep vals[i]
+    loss = np.where((vals <= 0 ), 0, vals)
+
+    weight_mag = np.linalg.norm(w)
+
+    return C * np.mean(loss) + 0.5 * weight_mag * weight_mag
+
+
+def test_svc_loss_convergence_and_time_scikit(type, datasets):
+    idx_count = 0
+    results = {}
+
+    for d in ds:
+        for n in ns:
+            X, y, _, _, _ = datasets[idx_count]
+
+            lvc = LinearSVC(dual=type, tol=TOLERANCE, C=C, max_iter=N_ITER)
+            start = time.perf_counter()
+            lvc.fit(X=X, y=y)
+            elapsed = time.perf_counter() - start
+            
+            losses = []
+            print(range(1, N_ITER, STEP))
+            for i in range(1,N_ITER, STEP):
+                lvc = LinearSVC(dual=type, tol=TOLERANCE, C=C, max_iter=i)
+                lvc.fit(X=X, y=y)
+                # print(f"np.shape(lvc.coeef_) = {np.shape(lvc.coef_)}")
+                # print(f"np.shape(X) = {np.shape(X)}")
+                # print(np.shape(X * lvc.coef_))
+                y_hat = np.dot(X, lvc.coef_.T)+ lvc.intercept_
+                # print(y_hat)
+                loss = hinge_loss(y=y, y_hat=y_hat, w=lvc.coef_, C=C)
+                print(f"loss: {loss}")
+                losses.append(loss)
+
+            results[(d, n)] = {
+                "losses": losses,
+                "fit_time": elapsed
+            }
+
+            idx_count += 1
+
+    return results
+
+def plot_loss_grid(results, type):
     fig, axs = plt.subplots(len(ds), len(ns), figsize=(18, 12), sharey=True)
-    fig.suptitle("Loss Convergence across datasets", fontsize=16)
+    fig.suptitle("Loss Convergence across datasets " + type, fontsize=16)
 
     fig.subplots_adjust(
         top=0.90,
@@ -104,18 +158,33 @@ def plot_loss_grid(results):
             ax.grid(True)
 
     plt.tight_layout(rect=[0, 0, 1, 0.95])
-    plt.savefig("plots/loss_convergence_grid.svg")
+    plt.savefig("plots/loss_convergence_grid" + type + ".svg")
     plt.show()
 
 if __name__ == "__main__":
+
     # test_C_values()
     datasets = generate_random_datasets(seed_input=11, u_input=1)
+
+    # OUR IMPLEMENTATION
     results = test_svc_loss_convergence_and_time(
         datasets=datasets,
-        C=15,
-        eta=0.01,
-        n_iter=100
     )
-    plot_loss_grid(results=results)
+    plot_loss_grid(results=results, type="Implemented")
+
+
+    # PRIMAL 
+    results = test_svc_loss_convergence_and_time_scikit(
+        datasets=datasets,
+        type=False #dual = false
+    )
+    plot_loss_grid(results=results, type="Primal")
+
+     # DUAL 
+    results = test_svc_loss_convergence_and_time_scikit(
+        datasets=datasets,
+        type=True #dual = true
+    )
+    plot_loss_grid(results=results, type="Dual")
 
     # print(datasets[0][0])
